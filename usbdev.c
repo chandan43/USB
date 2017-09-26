b8e8c965b1c743a009671f4b7654e9daf16bfe3f
Chandan <jha2011eec43@gmail.com>
2017-09-26 16:57:01 +0530

    Added interfaces function

--- usbdev.c
+++ usbdev.c
@@ -5,6 +5,7 @@
 #include <linux/usb.h>
 #include <linux/kref.h>
 #include <linux/types.h>
+#include <linux/errno.h>
 /*Driver INFO*/
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("beingchandanjha@gamil.com");
@@ -43,9 +44,49 @@ struct usb_dev {
 	size_t bulk_in_size;                   /*the size of the receive buffer */
 	__u8	bulk_in_endpointAddr;	/* the address of the bulk in endpoint */
 	__u8	bulk_out_endpointAddr;	/* the address of the bulk out endpoint */
-	struct kref kref;
+	struct kref kref;              
 };
+/*krefs allow you to add reference counters to your objects.  If you
+ * have objects that are used in multiple places and passed around, and
+ * you don't have refcounts, your code is almost certainly broken.  If
+ * you want refcounts, krefs are the way to go.*/
 
+/*Macro sets up a pointer that points to the struct device_driver passed to the code . The macro to get a pointer to struct usb_dev by using:*/
+#define to_usb_dev(d) container_of(d, struct usb_dev, kref);
+struct struct usb_driver usb_drv;
+static void usb_delete(struct kref *ref){
+	struct usb_dev *dev=to_usb_dev(kref);
+	usb_put_dev(dev->udev); /*release a use of the usb device structure.Must be called when a user of a device is finished with it*/
+	kfree(dev->bulk_in_buffer);  /*Free buffer*/
+	kfree (dev);   /*Free device*/
+}
+
+static int usb_open(struct inode *inodep, struct file *filep){
+	struct usb_dev *dev;
+	struct usb_interface *interface;
+	int subminor;
+	int retval=0;
+	/* Be sure to use iminor to obtain the minor number from the inode structure, and make sure that it corresponds to a device that your driver is actually prepared to handle.*/
+	subminor=iminor(inodep);   
+	interface=usb_find_interface(&usb_drv,subminor); /* find usb_interface pointer for driver and device */
+	if(!interface){
+		pr_err("%s: Can't find device for minor %d",__func__, subminor);
+		retval=-ENODEV;
+		goto exit;
+	}
+	dev = usb_get_intfdata(interface);	/* To retrieve the data, the function usb_get_intfdata should be called:*/
+	if(!dev){
+		retval=-ENODEV;
+		goto exit;
+	}
+	/* increment our usage count for the device */
+	kref_get(&dev->kref);
+	/* save our object in the file's private structure */
+	filep->private_data=dev;
+	return 0;
+exit:
+		return retval;
+}
 /* * @probe: Called to see if the driver is willing to manage a particular
  *      interface on a device.  If it is, probe returns zero and uses
  *      usb_set_intfdata() to associate driver-specific data with the
@@ -67,7 +108,7 @@ void usb_disconnect(struct usb_interface *intf){
 	pr_info("USB device is disconnected\n");
 }
 
-struct usb_driver usb_driver={
+struct usb_driver usb_drv={
 //	.owner = THIS_MODULE,
 	.name="usbdev", 
 	.id_table= usb_table,
@@ -77,12 +118,12 @@ struct usb_driver usb_driver={
 
 int __init usb_init(void){
 	pr_info("Initialization of USB driver\n");
-	usb_register(&usb_driver);
+	usb_register(&usb_drv);
 	return 0;
 }
 
 void __exit usb_exit(void){
-	usb_deregister(&usb_driver);
+	usb_deregister(&usb_drv);
 }
 
 module_init(usb_init);
