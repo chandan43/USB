f1e47bcec646d669a5aadf47af8fc69e826ad75b
Chandan <jha2011eec43@gmail.com>
2017-09-27 12:28:40 +0530

    Updated write function and file_operation

--- usbdev.c
+++ usbdev.c
@@ -7,7 +7,7 @@
 #include <linux/types.h>
 #include <linux/errno.h>
 #include <linux/slab.h>
-
+#include <asm/uaccess.h>
 /*Driver INFO*/
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("beingchandanjha@gamil.com");
@@ -16,7 +16,6 @@ MODULE_VERSION(".2");
 /* Define these values to match your devices */
 #define USB_SKEL_VENDOR_ID	0x0781
 #define USB_SKEL_PRODUCT_ID	0x5567
-
 /**
  * USB_DEVICE - macro used to describe a specific usb device
  * @vend: the 16 bit USB Vendor ID
@@ -125,7 +124,23 @@ static int  ssize_t usb_read(struct file *filep, char __user *buffer, size_t cou
 	}
 	return retval;
 }
+/* (in) completion routine */
+/*
+ *   - Out of memory (-ENOMEM)
+ *   - Unplugged device (-ENODEV)
+ *   - Stalled endpoint (-EPIPE)
+ *   - Too many queued ISO transfers (-EAGAIN)
+ *   - Too many requested ISO frames (-EFBIG)
+ *   - Invalid INT interval (-EINVAL)
+ *   - More than one packet for INT (-EINVAL)
+ */
+static void usb_write_bulk_callback(struct urb *urb){
+	if(urb->status && !(urb->status == -ENOENT || urb->status == -ECONNRESET ||urb->status == -ESHUTDOWN))
+		pr_debug("%s:  Nonzero write bulk status received: %d",__func__,urb->status);
+	/* free up our allocated buffer */
+	usb_free_coherent(urb->dev,urb->transfer_buffer_length,urb->transfer_buffer,urb->transfer_dma);
 
+}
 static ssize_t usb_write(struct file *filep, const char __user *buffer, size_t count, loff_t *offset){
 	struct usb_dev *dev;
 	int retval;
@@ -165,8 +180,19 @@ static ssize_t usb_write(struct file *filep, const char __user *buffer, size_t c
  * Initializes a bulk urb with the proper information needed to submit it
  * to a device.
  */
-      usb_fill_bulk_urb(urb,dev->udev,usb_sndbulkpipe(dev->udev,dev->bulk_out_endpointAddr),buf,count,,dev);
-	
+      usb_fill_bulk_urb(urb,dev->udev,usb_sndbulkpipe(dev->udev,dev->bulk_out_endpointAddr),buf,count,usb_write_bulk_callback,dev);
+	/*set URB_NO_TRANSFER_DMA_MAP so that usbcore won't map or unmap the buffer.*/
+      /*If short packets should NOT be tolerated, set URB_SHORT_NOT_OK in transfer_flags.*/
+      urb->transfer_flags |= URB_NO_TRANSFER_DMA_MAP;
+      /* send the data out the bulk port */
+      retval=usb_submit_urb(urb, GFP_KERNEL); //ON submit return 0.
+      if(retval){
+	      pr_err("%s: failed submitting write urb, error %d",__func__,retval);
+	      goto error;
+      }
+    /* release our reference to this urb, the USB core will eventually free it entirely */
+       usb_free_urb(urb);
+
 exit:
 	return count;
 error:
@@ -183,6 +209,30 @@ error:
  *      return -ENODEV, if genuine IO errors occurred, an appropriate
  *      negative errno value. */
 
+static struct file_operations usb_fops = {
+	.owner   = THIS_MODULE,
+	.read   = usb_read,
+	.write  = usb_write,
+	.open   = usb_open,
+	.release= usb_release,
+};
+/**
+ * struct usb_class_driver - identifies a USB driver that wants to use the USB major number
+ * @name: the usb class device name for this driver.  Will show up in sysfs.
+ * @devnode: Callback to provide a naming hint for a possible
+ *      device node to create.
+ * @fops: pointer to the struct file_operations of this driver.
+ * @minor_base: the start of the minor range for this driver.
+ *
+ * This structure is used for the usb_register_dev() and
+ * usb_unregister_dev() functions, to consolidate a number of the
+ * parameters used for them.
+ */
+struct usb_class_driver usb_class={
+	.name="usbdrv%d"
+	.fops=&usb_fops,
+	.minor_base = USB_SKEL_MINOR_BASE,
+};
 
 static int usb_probe(struct usb_interface *intf,const struct usb_device_id *id){
 	   pr_info("USB device  (%04X:%04X) is plugged\n", id->idVendor, id->idProduct);
